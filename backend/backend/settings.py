8a53699f39875f23ee106884aeaa9977ebb688b8
Mirlan <mirlan.kaparov24@gmail.com>
2022-10-12 02:46:50 +0600

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,16 +3,16 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-env_path = Path("../infra") / ".env"
+env_path = Path('../infra') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="default_secret_key")
+SECRET_KEY = os.getenv('SECRET_KEY', default='default_secret_key')
 
-DEBUG = os.getenv("DEBUG", default=False)
+DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.getenv("*",)
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -32,12 +32,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware', 
-    'django.contrib.sessions.middleware.SessionMiddleware', 
-    'django.middleware.common.CommonMiddleware', 
-    'django.middleware.csrf.CsrfViewMiddleware', 
-    'django.contrib.auth.middleware.AuthenticationMiddleware', 
-    'django.contrib.messages.middleware.MessageMiddleware', 
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -45,14 +45,14 @@ ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates', 
-        'DIRS': [], 
-        'APP_DIRS': True, 
-        'OPTIONS': { 
-            'context_processors': [ 
-                'django.template.context_processors.debug', 
-                'django.template.context_processors.request', 
-                'django.contrib.auth.context_processors.auth', 
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -62,26 +62,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
-    'default': { 
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'), 
-        'NAME': os.getenv('DB_NAME', default='postgres'), 
-        'USER': os.getenv('POSTGRES_USER', default='postgres'), 
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'), 
-        'HOST': os.getenv('DB_HOST', default='db'), 
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', 
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     }, 
     { 
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     }, 
     { 
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', 
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     }, 
     { 
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
