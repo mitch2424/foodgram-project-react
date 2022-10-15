f06fdce5881e6f1295dd177a314d35f31f1c46e1
Mirlan <mirlan.kaparov24@gmail.com>
2022-10-16 02:10:04 +0600

    '1'

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -73,7 +73,9 @@ class ShowFollowsSerializer(CustomUserSerializer):
     """Сериализатор отображения подписок."""
 
     recipes = serializers.SerializerMethodField()
-    recipes_count = serializers.IntegerField(source='recipes.count', read_only=True)
+    recipes_count = serializers.IntegerField(
+        source='recipes.count', read_only=True
+    )
 
     class Meta:
         model = User
